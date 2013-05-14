e039381b1b5abbd88e474c06df54f0ec2cfd8b26
David Bresnick <gravaton@gmail.com>
2013-05-14 11:03:26 -0400

    Shrank it again

--- Parsing/Scoring.rb
+++ Parsing/Scoring.rb
@@ -2,7 +2,8 @@
 
 def mjScore(fan, hu, d, t = false)
 	#print ((([(hu * (2**(fan + 2))).fdiv(1000),2,3,3,4,4,4,6,6,8].sort[[0,fan - 5,8].sort[1]] * 10).instance_exec(t,d) { |t,d| [ (t ? 2 : d ? 6 : 4) * self, (d ? 2 : 1) * self][0,t ? 2 : 1] } ).map { |i| i.ceil * 100 }).join(" / "), "\n"
-	print (Array.new(t ? 2 : 1,(([(hu * (2**(fan + 2))).fdiv(1000),2,3,3,4,4,4,5,5,8].sort[[0,fan - 5,8].sort[1]] * 10))).map.with_index { |n,i| ([(t ? 2 : d ? 6 : 4), (d ? 2 : 1)][i] * n).ceil * 100}).join(" / "), "\n"
+	#print (Array.new(t ? 2 : 1,(([(hu * (2**(fan + 2))).fdiv(1000),2,3,3,4,4,4,5,5,8].sort[[0,fan - 5,8].sort[1]] * 10))).map.with_index { |n,i| ([(t ? 2 : d ? 6 : 4), (d ? 2 : 1)][i] * n).ceil * 100}).join(" / "), "\n"
+	print (Array.new(t ? 2 : 1,(([(hu * (2**(fan + 2))).fdiv(1000),2,3,3,4,4,4,5,5,8].sort[[0,fan - 5,8].sort[1]] * 10))).zip([(t ? 2 : d ? 6 : 4), (d ? 2 : 1)]).map { |x| x.reduce(:*).ceil * 100 }).join(" / "), "\n"
 end
 
 mjScore(2,40,true,true)
